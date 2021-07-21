1051c222c3adf931cba620a8c2b4647c740b4ff7
Carlos Alvarez <carlosxviii@gmail.com>
2021-07-21 12:52:25 -0300

    moved to function

---
+++ js/emulators.js
@@ -0,0 +1,13 @@
+
+
+
+function nes(player) {
+    EJS_player = player;
+    EJS_biosUrl = ''; // Url to Famicom Disk System bios
+    EJS_gameUrl = 'https://www.emulatorjs.com/roms/mega_mountain.nes'; // Url to Game rom
+    EJS_gameID = 1; // ID in your website, required for netplay.
+    EJS_core = 'nes';
+    EJS_lightgun = false; // Lightgun
+}
+
+
